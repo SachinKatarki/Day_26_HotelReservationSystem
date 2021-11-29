86422b27256f942f31c6ab9fa10098b94a45782d
Textspot <sachinkatarki1995@gmail.com>
2021-11-29 19:43:59 +0530

    Adde:Customer need to Find the Chpeast Hotel in the Hotel Reservation System

--- src/test/java/com/bridgelabz/hotelreservationsystem/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservationsystem/HotelReservationTest.java
@@ -4,12 +4,14 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 
 public class HotelReservationTest {
-	   @Test
+	 @Test
 	    public void whenHotelAdded_ToSystem_ShouldGetAdded() {
 	        Hotel hotel1 = new Hotel("Lakeewood", 110, 90, 80, 80, 3);
 	        Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
@@ -23,5 +25,18 @@ public class HotelReservationTest {
 	        List<Hotel> result = hotelReservation.getHotelList();
 	        assertEquals(hotels, result);
 	    }
-
+	    @Test
+	    public void whenGivenDateRangeShouldReturnCheapestHotel()
+	    {
+	        Hotel hotel1 = new Hotel("Lakewood", 110, 90, 80, 80, 3);
+	        Hotel hotel2 = new Hotel("Bridgewood", 160, 60, 110, 50, 4);
+	        Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 100, 40, 5);
+	        HotelReservation hotelReservation = new HotelReservation();
+	        hotelReservation.add(hotel1);
+	        hotelReservation.add(hotel2);
+	        hotelReservation.add(hotel3);
+	        Map<Integer, Hotel> result = hotelReservation.searchFor("10Sep2020", "11Sep2020");
+	        result.forEach((k, v) -> System.out.println(v.getName() + " " + k));
+	        assertNotNull(result);
+	    }
 }
